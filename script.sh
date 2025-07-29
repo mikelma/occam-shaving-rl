b404e58bee8af5ae34399435031cb7e3bdef85f1
Seth Akins <sethlinc4@gmail.com>
2025-07-29 13:43:00 -0600

    updated script

--- script.sh
+++ script.sh
@@ -8,7 +8,13 @@
 
 echo "Starting task $SLURM_ARRAY_TASK_ID"
 module load python/3.13 gcc opencv mujuco
-ls -la
+
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+python -m pip install --no-index --upgrade pip
+
+python -m pip install --no-index -r compute-canada-requirements.txt
 
 export WANDDB_MODE="offline"
 
+python ppo_continuous_action.py
