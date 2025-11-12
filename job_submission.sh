d784b798635314aa8ac5c9f34d93694f51cc4a6a
Seth Akins <sethlinc4@gmail.com>
2025-11-12 15:59:18 -0700

    added job submission file

---
+++ job_submission.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=rrg-mbowling-ad
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus=nvidia_h100_80gb_hbm3_1g.10gb:1
+#SBATCH --mem=16000M
+#SBATCH --time=02:59:00
+#SBATCH --output=job_testing/job_testing_%A_%a.out
+
+module load python/3.12 mujoco/3.3.0
+
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+python -m pip install --no-index --upgrade pip
+
+python -m pip download -d $SLURM_TMPDIR/wheels --no-deps flashbax navix rlax==0.1.6
+
+python -m pip install -U --no-index --find-links $SLURM_TMPDIR/wheels/ -r updated-reqs.txt
+python ppo_continuous_action.py
