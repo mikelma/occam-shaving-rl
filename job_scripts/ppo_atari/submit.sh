35fd6479f885d0175f40209a6bebdba8cb3c5609
Seth Akins <sethlinc4@gmail.com>
2025-11-26 17:26:39 -0700

    added script

---
+++ job_scripts/ppo_atari/submit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=rrg-mbowling-ad
+#SBATCH --cpus-per-task=8
+#SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1
+#SBATCH --mem=32000M
+#SBATCH --time=8:59:00
+#SBATCH --output=ppo_atari/ppo_atari_%A_%a.out
+
+module load python/3.12 mujoco/3.3.0
+
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+python -m pip install --no-index --upgrade pip
+
+python -m pip install -r requirements/requirements-envpool.txt
+python -m pip install -r requirements/requirements-jax.txt
+python -m pip install --upgrade "jax[cuda]==0.3.17" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+python cleanrl/ppo_atari_envpool_xla_jax_scan.py --env-id Breakout-v5
