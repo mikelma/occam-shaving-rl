cfdbf074dec62d800ac6c3324b74c7192996ad72
Seth Akins <sethlinc4@gmail.com>
2025-12-04 16:04:57 -0700

    modified submission script to run on vulcan

--- job_scripts/ppo_atari/vulcan/submit.sh
+++ job_scripts/ppo_atari/vulcan/submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --account=rrg-mbowling-ad
+#SBATCH --account=aip-mbowling
 #SBATCH --cpus-per-task=8
-#SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1
+#SBATCH --gpus-per-node=1
 #SBATCH --mem=16000M
-#SBATCH --time=2:59:00
+#SBATCH --time=8:59:00
 #SBATCH --output=ppo_atari/ppo_atari_%A_%a.out
 
 module load python/3.10 cuda
