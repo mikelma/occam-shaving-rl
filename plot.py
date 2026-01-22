8f5966dfccbdd321a5335a64cab7cff6fc16923e
Mikel <mikelma@tutanota.com>
2026-01-22 17:59:26 +0100

    add config file to plot.py

--- plot.py
+++ plot.py
@@ -4,6 +4,7 @@
 #     "seaborn",
 #     "pandas",
 #     "tyro",
+#     "msgpack",
 # ]
 # ///
 
@@ -11,12 +12,25 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import tyro
+import msgpack
+import pprint
 
 
-def main(file: str):
+def main(file: str, cfg: str):
+    f = open(cfg, "rb")
+    bin = f.read()
+    f.close()
+    configs = msgpack.unpackb(bin, raw=False)
+
+    run_id = int(file.split("/")[-1].split("_")[-1].split(".")[0])
+    config = configs[run_id]
+
+    pprint.pp(config)
+
     df = pd.read_csv(file)
 
     sns.lineplot(data=df, x="step", y="episodic_return", estimator=None, units="seed")
+    plt.title(config["ENV_NAME"])
     plt.show()
 
 
