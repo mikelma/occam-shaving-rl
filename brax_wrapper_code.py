495c62f9a7987222f7ab3f8099e13ba8753f43ea
Seth Akins <sethlinc4@gmail.com>
2026-02-05 17:31:39 -0700

    Add randomized reset wrapper

---
+++ brax_wrapper_code.py
@@ -0,0 +1,45 @@
+import jax
+import jax.numpy as jnp
+import chex
+import numpy as np
+from flax import struct
+from functools import partial
+from typing import Optional, Tuple, Union, Any
+from gymnax.environments import environment, spaces
+from brax import envs
+from brax.envs.wrappers.training import EpisodeWrapper, AutoResetWrapper
+from brax.envs.base import Env, State, Wrapper
+
+from wrappers import BraxGymnaxWrapper
+
+
+class RandomizedAutoResetWrapper(Wrapper):
+    """
+    Automatically resets Brax envs that are done. and mask the goal observation.
+    Force resample every step.
+    """
+    def reset(self, rng: jnp.ndarray) -> State:
+        rng, _rng = jax.random.split(rng)
+        state = self.env.reset(_rng)
+        state.info['rng'] = rng
+        state.info['pipeline_state'] = state.pipeline_state
+        state.info['obs'] = state.obs
+        return state
+
+    def step(self, state: State, action: jnp.ndarray) -> State:
+        if 'steps' in state.info:
+            steps = state.info['steps']
+            steps = jnp.where(state.done, jnp.zeros_like(steps), steps)
+            state.info.update(steps=steps)
+            state = state.replace(done=jnp.zeros_like(state.done))
+            state = self.env.step(state, action)
+            maybe_reset = self.reset(state.info['rng'])
+            def where_done(x, y):
+                done = state.done
+                if done.shape:
+                    done = jnp.reshape(done, [x.shape[0]] + [1] * (len(x.shape) - 1)) # type: ignore
+                return jnp.where(done, x, y)
+            pipeline_state = jax.tree_util.tree_map(
+            where_done, maybe_reset.info['pipeline_state'], state.pipeline_state        )
+            obs = where_done(maybe_reset.info['obs'], state.obs)
+            return state.replace(pipeline_state=pipeline_state, obs=obs)
