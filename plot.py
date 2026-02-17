1a43559f884890427ad27812df32d69bd4395899
Mikel <mikelma@tutanota.com>
2026-02-17 11:42:40 +0100

    Add hyper-plot to plot.py

--- plot.py
+++ plot.py
@@ -43,30 +43,14 @@ def config_to_vec(cfg, subkey):
         options = config.META_CONFIG[subkey][key]
         if isinstance(options, list):
             idx = options.index(value)
-            v.append(idx)
-    # v = []
-    # for key, value in cfg.items():
-    #     if isinstance(value, bool):
-    #         v.append(int(value))
-
-    #     elif isinstance(value, int) or isinstance(value, float):
-    #         v.append(value)
-
-    #     elif key in ["ACTIVATION", "INITIALIZERS", "INITIALIZERS", "ENV_NAME"]:
-    #         idx = config.META_CONFIG[subkey][key].index(value)
-    #         v.append(idx)
-
-    #     elif key == "LOG_DIR":
-    #         pass
-
-    #     else:
-    #         print(key, value)
-    #         raise NotImplementedError()
+            one_hot = [0]*len(options)
+            one_hot[idx] = 1.
+            v += one_hot
     return np.array(v)
 
 
 @app.command
-def perf_distrib(cfg: str, dir: str):
+def hyper_plot(cfg: str, dir: str):
     configs = load_config(cfg)
 
     if "minatar" in dir:
@@ -91,7 +75,7 @@ def perf_distrib(cfg: str, dir: str):
 
         if equal:
             print("Found default config!")
-            config_id = i
+            config_id =i
 
         cfg_vecs.append(config_to_vec(cfg, subkey))
 
@@ -101,25 +85,17 @@ def perf_distrib(cfg: str, dir: str):
         df = df.with_columns(pl.lit(run_id).alias("id"))
         dfs.append(df)
 
-        i += 1
-        if i == 100:
-            break
-
     config_id = 12
     assert config_id is not None, "Cannot find default config"
     print("Default config id:", config_id)
 
     cfg_vecs = np.vstack(cfg_vecs)
+    print(cfg_vecs)
     print("Shape of config vectors:", cfg_vecs.shape)
 
-    # vecs_min = cfg_vecs.min(axis=0)
-    # vecs_max = cfg_vecs.max(axis=0)
-    # cfg_vecs = (cfg_vecs - vecs_min) / (vecs_max - vecs_min)
-
-    # # FIX as we're using the index of hyperparameters: option 2 is closer to option 3 than to option 5 (we have to fix this)
-    # cfg_low_dim = TSNE(
-    #     n_components=2, learning_rate="auto", init="random", perplexity=3
-    # ).fit_transform(cfg_vecs)
+    cfg_low_dim = TSNE(
+        n_components=2, learning_rate="auto", init="random", perplexity=3
+    ).fit_transform(cfg_vecs)
 
     # cols: id, seed, auc
     df = pl.concat(dfs)
@@ -136,11 +112,66 @@ def perf_distrib(cfg: str, dir: str):
     # print(cfg_low_dim.shape)
     print(df_norm)
 
-    # plt.scatter(x=cfg_low_dim[:, 0], y=cfg_low_dim[:, 1], c=df_norm["auc_norm"])
-    # cbar = plt.colorbar()
-    # cbar.set_label("Normalized performance", rotation=270)
-    # plt.xlabel("X1")
-    # plt.ylabel("X2")
+    plt.scatter(x=cfg_low_dim[:, 0], y=cfg_low_dim[:, 1], c=df_norm["auc_norm"])
+    cbar = plt.colorbar()
+    cbar.set_label("Normalized performance", rotation=270)
+    plt.xlabel("X1")
+    plt.ylabel("X2")
+
+    # defaults_perf = df_norm.filter(pl.col("id") == config_id)["auc_norm"].item()
+    # sns.kdeplot(data=df_norm, x="auc_norm")
+    # plt.axvline(x=defaults_perf, color="tab:grey", label="baseline")
+    # plt.legend()
+    # plt.xlabel("Normalized performance")
+    plt.show()
+
+
+@app.command
+def perf_distrib(cfg: str, dir: str):
+    configs = load_config(cfg)
+
+    if "minatar" in dir:
+        default_config = config.META_CONFIG["minatar_baseline"]
+    else:
+        raise NotImplementedError()
+
+    dfs = []
+    i = 0
+    config_id = None
+    for i, path in tqdm(enumerate(glob.glob(dir + "/*/*"))):
+        run_id = int(path.split("/")[-1].split("_")[-1].split(".")[0])
+
+        cfg = configs[run_id]
+        equal = True
+        for key, value in default_config.items():
+            if cfg[key] != value:
+                equal = False
+                break
+
+        if equal:
+            print("Found default config!")
+            config_id =i
+
+        df = pl.read_csv(path)
+
+        df = df.group_by("seed").agg(pl.col("episodic_return").sum().alias("auc"))
+        df = df.with_columns(pl.lit(run_id).alias("id"))
+        dfs.append(df)
+
+    assert config_id is not None, "Cannot find default config"
+    print("Default config id:", config_id)
+
+    # cols: id, seed, auc
+    df = pl.concat(dfs)
+
+    # compute normalized auc
+    q_5 = df.select(pl.col("auc").quantile(0.05)).item()
+    q_95 = df.select(pl.col("auc").quantile(0.95)).item()
+    df_norm = (
+        df.group_by("id")
+        .agg(pl.col("auc").mean())
+        .with_columns(((pl.col("auc") - q_5) / (q_95 - q_5)).alias("auc_norm"))
+    )
 
     defaults_perf = df_norm.filter(pl.col("id") == config_id)["auc_norm"].item()
     sns.kdeplot(data=df_norm, x="auc_norm")
